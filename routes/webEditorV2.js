d379cdf79c555d77a6dc92a807b65a98f0cfbe7c
阿星 <jiangzengxing@xiaomi.com>
2019-10-14 11:34:23 +0800

    提交

--- routes/webEditorV2.js
+++ routes/webEditorV2.js
@@ -1,9 +1,17 @@
 const Router = require('koa-router');
+const fs = require('fs');
+const axios = require('axios');
 const { EditorModel } = require('../src/model/webEditorV2');
 
+const templateHtml = fs.readFileSync('./src/ssr/weditor.temp.html');
+const moduleUrl = 'https://jzxznb.github.io/Container.js';
 const webEditorV2 = new Router({
     prefix: '/webEditorV2',
 });
+const importModule = async () => {
+    const r = await axios.get(moduleUrl);
+    return r.data;
+};
 const FAILED = '失败,请联系阿星或稍后再试';
 const htmlTemp = '';
 webEditorV2
@@ -17,7 +25,11 @@ webEditorV2
     .get('/getHomePage/:pageId', async (ctx) => {
         const { pageId } = ctx.params;
         const res = await EditorModel.find({ _id: pageId });
-        ctx.body = res[0].body;
+        const Container = await importModule();
+        // const rr = require(Object(Container).default);
+        console.log(JSON.parse(Container));
+        ctx.response.body = Container;
+        // ctx.body = templateHtml.toString();
     });
 module.exports = {
     webEditorV2,
