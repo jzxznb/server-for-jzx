dc87b79f8c15391335d11d626c6a88267d6233b9
阿星 <jiangzengxing@xiaomi.com>
2019-11-19 20:02:31 +0800

    增加mode

--- ssrConfig/webpack.common.js
+++ ssrConfig/webpack.common.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const baseWebpackConfig = {
+    mode: 'production',
     resolve: {
         extensions: ['.js', '.jsx', '.json']
     },
